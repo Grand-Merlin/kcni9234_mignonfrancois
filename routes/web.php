04c32779703e2a671fe117fd046601ac89fcefd5
François <mignon.francois@gmail.com>
2023-05-20 17:56:12 +0200

    commented out the login route in web.php

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/register', function () {
     return view('register');
 })->name('register');
-
+/*
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+*/
 
