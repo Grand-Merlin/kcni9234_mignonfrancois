611fb659901c1074ec9573ab1efb854982308248
François <mignon.francois@gmail.com>
2023-05-20 17:35:16 +0200

    Reverting to login view on '/' route.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
-})->name('login');
-
-
-Route::get('/', function () {
     return view('register');
 })->name('register');
+
+Route::get('/', function () {
+    return view('login');
+})->name('login');
 
