bbdfec07e985f61a11389ad3e2690fddc6624c2b
François <mignon.francois@gmail.com>
2023-05-21 18:03:03 +0200

    Ajout d'un formulaire d'inscription avec thème sombre et validation de la csrf.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,37 +1,70 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+    <!-- Définit le titre de la page -->
     <title>Formulaire d'inscription</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Lie la feuille de style CSS à la page HTML -->
+    <link rel="stylesheet" href="{{ asset('../../../css/styles.css') }}">
 </head>
+
 <body>
-    <div id="registration-form">
-        <h2>Inscription</h2>
-        <form action="#">
-            <label for="email">Email*:</label><br>
-            <input type="email" id="email" name="email" required><br>
-            <label for="email-verification">Email de vérification*:</label><br>
-            <input type="email" id="email-verification" name="email-verification" required><br>
-
-            <label for="password">Mot de passe*:</label><br>
-            <input type="password" id="password" name="password" required><br>
-            <label for="password-verification">Vérification du mot de passe*:</label><br>
-            <input type="password" id="password-verification" name="password-verification" required><br>
-
-            <label for="fullname">Nom Prénom*:</label><br>
-            <input type="text" id="fullname" name="fullname" required><br>
-
-            <label for="address">Adresse*:</label><br>
-            <textarea id="address" name="address" required></textarea><br>
-
-            <label for="country">Pays:</label><br>
-            <input type="text" id="country" name="country"><br>
-
-            <label for="locality">Localité*:</label><br>
-            <input type="text" id="locality" name="locality" required><br>
-
-            <input type="submit" value="Inscription">
-        </form>
+    <div id="background-div">
+        <div class="background"></div>
+        <div class="background background_dark"></div>
     </div>
+
+    <div id="content-div">
+        <!-- Ajoute un bouton bascule pour le thème sombre -->
+        <input type="checkbox" id="toggle_checkbox">
+        <label for="toggle_checkbox">
+            <div id="star">
+                <div class="star" id="star-1">★</div>
+                <div class="star" id="star-2">★</div>
+            </div>
+            <div id="moon"></div>
+        </label>
+        <div id="registration-form">
+            <h2>Inscription</h2>
+            <form action="/register" method="post">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                @csrf
+                <div class="input-container">
+                    <div>
+                        <label for="email">Email*:</label><br>
+                        <input type="email" id="email" name="email" required><br>
+                        <label for="email-verification">Email de vérification*:</label><br>
+                        <input type="email" id="email-verification" name="email-verification" required><br>
+
+                        <label for="password">Mot de passe*:</label><br>
+                        <input type="password" id="password" name="password" required><br>
+                        <label for="password-verification">Vérification du mot de passe*:</label><br>
+                        <input type="password" id="password-verification" name="password-verification" required><br>
+
+                        <label for="fullname">Nom Prénom*:</label><br>
+                        <input type="text" id="fullname" name="fullname" required><br>
+
+                        <label for="address">Adresse*:</label><br>
+                        <textarea id="address" name="address" required></textarea><br>
+
+                        <label for="country">Pays:</label><br>
+                        <input type="text" id="country" name="country"><br>
+
+                        <label for="locality">Localité*:</label><br>
+                        <input type="text" id="locality" name="locality" required><br>
+                    </div>
+                </div>
+                <div class="button-container">
+                    <input type="submit" value="Inscription">
+                    <button onclick="window.location.href='login'">Se connecter</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    <!-- Lie le fichier JavaScript à la page HTML -->
+    <script src="{{ asset('../../../js/main.js') }}"></script>
+
 </body>
+
 </html>
