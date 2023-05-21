24bdb77f3270fe516a9efe021e0006bfcc5a894b
François <mignon.francois@gmail.com>
2023-05-21 18:27:16 +0200

    Ajout des champs d'inscription pour email, mot de passe et adresse. (87 caractères)

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -25,33 +25,30 @@
             <div id="moon"></div>
         </label>
         <div id="registration-form">
+            <h2>Inscription</h2>
             <form action="/registerUser" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 @csrf
                 <div class="input-container">
                     <div>
-                        <label for="email">Email*:</label><br>
+                        <label for="email">Email*:</label>
+                        <label for="email-verification">Email de vérification*:</label>
+                        <label for="password">Mot de passe*:</label>
+                        <label for="password-verification">Vérification du mot de passe*:</label>
+                        <label for="fullname">Nom Prénom*:</label>
+                        <label for="address">Adresse*:</label>
+                        <label for="country">Pays:</label>
+                        <label for="locality">Localité*:</label>
+                    </div>
+                    <div>
                         <input type="email" id="email" name="email" required><br>
-                        <label for="email-verification">Email de vérification*:</label><br>
                         <input type="email" id="email-verification" name="email-verification" required><br>
-
-                        <label for="password">Mot de passe*:</label><br>
                         <input type="password" id="password" name="password" required><br>
-                        <label for="password-verification">Vérification du mot de passe*:</label><br>
                         <input type="password" id="password-verification" name="password-verification" required><br>
-
-                        <label for="fullname">Nom Prénom*:</label><br>
                         <input type="text" id="fullname" name="fullname" required><br>
-
-                        <label for="address">Adresse*:</label><br>
                         <textarea id="address" name="address" required></textarea><br>
-
-                        <label for="country">Pays:</label><br>
                         <input type="text" id="country" name="country"><br>
-
-                        <label for="locality">Localité*:</label><br>
                         <input type="text" id="locality" name="locality" required><br>
-                        
                         <input type="checkbox" id="terms" name="terms" required>
                         <label for="terms">J'accepte les <a href="/terms-and-conditions">Conditions Générales</a>*</label><br>
                     </div>
