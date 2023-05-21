04daaa9377f8335a0d3e3e13a3c90c85c5fb903f
François <mignon.francois@gmail.com>
2023-05-21 13:26:21 +0200

    "Log des connexions réussies/échouées"

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
@@ -16,32 +19,38 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        //TODO: Use your custom form request or use the request helper
-    
         // Validating input
         $request->validate([
             'EMAIL' => 'required|email',
             'PASSWORD' => 'required'
         ]);
-    
+
         // Get user by email
-        $user = User::where('EMAIL', $request->email)->first();
-    
+        $user = User::where('EMAIL', $request->EMAIL)->first();
+
         // Check if user exists and the hashed password matches the given password
-        if ($user && md5($request->password) === $user->PASSWORD) {
+        if ($user && Hash::check($request->PASSWORD, $user->PASSWORD)) {
+            // Log successful login
+            Log::info('User logged in: ', ['email' => $request->EMAIL]);
+
             // Manual login
             Auth::login($user);
             return redirect(RouteServiceProvider::IMMOBILIER);
         }
-    
+
+        // Log failed login attempt
+        Log::warning('Failed login attempt: ', ['email' => $request->EMAIL]);
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
     }
-    
 
     public function logout(Request $request)
     {
+        // Log successful logout
+        Log::info('User logged out: ', ['email' => Auth::user()->EMAIL]);
+
         Auth::logout();
         return redirect('/login');
     }
