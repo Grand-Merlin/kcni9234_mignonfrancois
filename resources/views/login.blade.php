371a124a91db26bb436e0182482b99de3ca80fe3
François <mignon.francois@gmail.com>
2023-05-22 21:55:55 +0200

    modification de l'URL pour les images de fond (moins de 100 caractères)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Formulaire</title>
-    <link href="{{ asset('../css/styles.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="form-container">
