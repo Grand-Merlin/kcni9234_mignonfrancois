489b3f0df1f478ee22919ea05928c28c02a0bfd3
François <mignon.francois@gmail.com>
2023-05-22 21:39:29 +0200

    "Corrige la fonction de login et ajoute une redirection"

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -36,6 +36,7 @@ class LoginController extends Controller
             // Manual login
             Auth::login($user);
             return redirect(RouteServiceProvider::IMMOBILIER);
+            
         }
     
         // Log failed login attempt
