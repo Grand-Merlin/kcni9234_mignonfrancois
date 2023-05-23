978da7080e59f1186a227c0a978a053aa08c6a92
François <mignon.francois@gmail.com>
2023-05-24 00:11:03 +0200

    Ajout de routes pour l'immobilier et les propriétés.

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,21 @@ Route::get('/register', [RegisterUserController::class, 'showForm']);
 Route::post('/devweb', [LoginController::class, 'login']); // Formulaire de connexion
 Route::post('/logout', [LoginController::class, 'logout']); // Route pour déconnecter l'utilisateur
 
+// Page d'accueil
+Route::get('/immobilier', [PropertyController::class, 'index'])->name('immobilier');
+
+// Ajout de biens
+// Note: Ceci nécessiterait probablement une méthode POST également pour traiter le formulaire d'ajout
+// Route::post('/immobilier/add', [PropertyController::class, 'add']);
+
+// Déconnexion
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Mes biens
+Route::get('/myProperties', [PropertyController::class, 'myProperties'])->name('myProperties');
+
+// Tous les biens
+Route::get('/allProperties', [PropertyController::class, 'allProperties'])->name('allProperties');
+
+// Filtrer les propriétés
+Route::get('/filterProperties/{filter}', [PropertyController::class, 'filterProperties'])->name('filterProperties');
